aeb84ea9586bc706ae149c58f41b5a970fef72e8
sinfulsamurai <maxim.gayduchek@gmail.com>
2021-09-03 00:34:45 +0300

    Fixed timezone

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -146,7 +146,7 @@ public class Main extends TelegramLongPollingBot {
                 String time = FORMAT_TIME.format(now);
 
                 switch (time) {
-                    case "04:00" -> sendSchedule();
+                    case "07:00" -> sendSchedule();
                     default -> {
                         List<Lecture> lectureList = getLectures();
 
